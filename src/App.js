3a939ea598c40692e547633a550b02bc013de3f1
Wisdom Liu <wisdom.code161@gmail.com>
2019-05-26 14:56:25 -0700

    created fillBar function

--- src/App.js
+++ src/App.js
@@ -10,13 +10,33 @@ class App extends Component {
     this.state = {
       percentage: 0
     }
+    this.fillBar = this.fillBar.bind(this)
+  }
+
+  fillBar() {
+    let atPercent = 0;
+    const interval = setInterval(() => {
+      atPercent++;
+      if (atPercent >= 100) {
+        clearInterval(interval);
+      }
+    }, 1000)
+
+    this.setState((prevState) => {
+      console.log(atPercent)
+      return {
+        percentage: atPercent
+      }
+    });
+  
   }
 
   render() {
     return(
       <div className="app">
-        <h1 id="title">My React Progress Bar!</h1>
+        <h1 id="title">My React Progress Bar</h1>
         <ProgressBar percentage={this.state.percentage}/>
+        <button onClick={this.fillBar}>Start</button>
       </div>
     )
   }
